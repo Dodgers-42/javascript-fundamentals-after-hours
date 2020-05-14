fd209dd21cedab647909a92d2219dd75922835fb
Vincent Sanders <vincesanders@hotmail.com>
2020-05-14 14:37:12 -0500

    Added challenge for classes.js

---
+++ classes.js
@@ -0,0 +1,12 @@
+// Task 1: Copy and paste your prototype in here and refactor into class syntax.
+
+// Test your area and circumference methods by uncommenting the logs below:
+// console.log(circle.area()); // 12.566370614359172
+// console.log(circle.circumference()); // 25.132741228718345
+
+// Task 2: Extend the base class CircleMaker with a sub class called SphereMaker.  Find out the formulas for volume and (surface)area for spheres and create those methods using the diameter property from CircleMaker.  Test your work by logging out your volume and area.
+
+// Test your work by uncommenting the logs below (diameter is 5):
+// console.log(sphere.area()); //78.53981633974483
+// console.log(sphere.circumference()); // 25.132741228718345
+// console.log(sphere.volume()); // 65.44984694978734
